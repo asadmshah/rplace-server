8bb7c8470442ce9bcadf7d14fba678f50ea7635d
Asad Shah <me@asadmshah.com>
2017-06-12 01:27:16 +0500

    StreamHandler only uses handles Binary messages.

--- server/src/main/kotlin/com/asadmshah/rplace/server/handlers/StreamHandler.kt
+++ server/src/main/kotlin/com/asadmshah/rplace/server/handlers/StreamHandler.kt
@@ -1,7 +1,6 @@
 package com.asadmshah.rplace.server.handlers
 
 import com.asadmshah.rplace.models.DrawEvent
-import com.asadmshah.rplace.models.Position
 import com.asadmshah.rplace.pubsub.PubSubClient
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -33,7 +32,7 @@ internal class StreamHandler @Inject constructor(private val pubSub: PubSubClien
                 .observeOn(Schedulers.from(channel.ioThread.worker))
                 .subscribe(
                         { events ->
-                            WebSockets.sendText(events.toString(), channel, null)
+                            WebSockets.sendBinary(events.toByteString().asReadOnlyByteBuffer(), channel, null)
                         },
                         { throwable: Throwable ->
                             throwable.printStackTrace()
@@ -46,11 +45,23 @@ internal class StreamHandler @Inject constructor(private val pubSub: PubSubClien
                 val data = BufferedBinaryMessage(64, false)
                 data.read(messageChannel, object : WebSocketCallback<BufferedBinaryMessage> {
                     override fun complete(channel: WebSocketChannel, context: BufferedBinaryMessage) {
-                        val buffer = toBuffer(*context.data.resource)
-                        val event = DrawEvent.parseFrom(buffer)
+                        if (context.data.resource.isNotEmpty()) context.data.resource.let { payload ->
+                            val size = Buffers.remaining(payload).toInt()
+                            if (size > 0) {
+                                val buffer = ByteBuffer.allocate(size)
+                                payload.forEach { buffer.put(it) }
+                                buffer.flip()
 
-                        pubSub.publish(event)
-                        buffer.clear()
+                                try {
+                                    pubSub.publish(DrawEvent.parseFrom(buffer))
+                                } catch (e: Exception) {
+                                    LOGGER.error("Unable to Publish Message.", e)
+                                    channel.close()
+                                } finally {
+                                    buffer.clear()
+                                }
+                            }
+                        }
                     }
 
                     override fun onError(channel: WebSocketChannel, context: BufferedBinaryMessage, throwable: Throwable) {
@@ -59,45 +70,6 @@ internal class StreamHandler @Inject constructor(private val pubSub: PubSubClien
                         channel.close()
                     }
                 })
-            }
-
-            override fun onText(webSocketChannel: WebSocketChannel, messageChannel: StreamSourceFrameChannel) {
-                val data = BufferedTextMessage(64, false)
-                data.read(messageChannel, object : WebSocketCallback<BufferedTextMessage> {
-                    override fun complete(channel: WebSocketChannel, context: BufferedTextMessage) {
-                        val split = context.data.split(":")
-                        if (split.size == 4) {
-                            val x = split[0].toInt()
-                            val y = split[1].toInt()
-                            val c = split[2].toInt()
-                            val d = split[3].toLong()
-                            val position = Position.newBuilder().setX(x).setY(y).build()
-                            val event = DrawEvent.newBuilder().setPosition(position).setColor(c).setDatetime(d).build()
-
-                            pubSub.publish(event)
-                        }
-                    }
-
-                    override fun onError(channel: WebSocketChannel, context: BufferedTextMessage, throwable: Throwable) {
-                        LOGGER.error("onTextRead Error", throwable)
-
-                        channel.close()
-                    }
-                })
-            }
-
-            private fun toBuffer(vararg payload: ByteBuffer): ByteBuffer {
-                if (payload.isEmpty()) return payload[0]
-
-                val size = Buffers.remaining(payload).toInt()
-                if (size == 0) {
-                    return Buffers.EMPTY_BYTE_BUFFER
-                }
-
-                val buffer = ByteBuffer.allocate(size)
-                payload.forEach { buffer.put(it) }
-                buffer.flip()
-                return buffer
             }
         })
 
